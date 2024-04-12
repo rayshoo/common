468c9ed081a3d168c4f6ab6797f602f042016ac3
rayshoo <bcchoi@deleo.co.kr>
2024-04-12 12:28:19 +0900

    [ADD] add combine string with separator func

--- strings.go
+++ strings.go
@@ -13,3 +13,19 @@ func CombineString(strings ...string) (*string, error) {
 	result := builder.String()
 	return &result, nil
 }
+
+func CombineStringWithSeparator(c rune, strings ...string) (*string, error) {
+	var builder s.Builder
+	for i := range strings {
+		_, err := builder.WriteString(strings[i])
+		if err != nil {
+			return nil, err
+		}
+		_, err = builder.WriteRune(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := builder.String()
+	return &result, nil
+}
