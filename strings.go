630131134f9e69b29be3750e7eb8a002c5511e8b
rayshoo <bcchoi@deleo.co.kr>
2024-04-12 12:42:52 +0900

    [ADD] add combine string refferer functions

--- strings.go
+++ strings.go
@@ -14,14 +14,73 @@ func CombineString(strings ...string) (*string, error) {
 	return &result, nil
 }
 
-func CombineStringWithSeparator(c rune, strings ...string) (*string, error) {
+func CombineStringReferrer(strings *[]string) (*string, error) {
+	var builder s.Builder
+	for i := range *strings {
+		_, err := builder.WriteString((*strings)[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := builder.String()
+	return &result, nil
+}
+
+func CombineStringWithRuneSeparator(sep rune, strings ...string) (*string, error) {
 	var builder s.Builder
 	for i := range strings {
 		_, err := builder.WriteString(strings[i])
 		if err != nil {
 			return nil, err
 		}
-		_, err = builder.WriteRune(c)
+		_, err = builder.WriteRune(sep)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := builder.String()
+	return &result, nil
+}
+
+func CombineStringWithStringSeparator(sep string, strings ...string) (*string, error) {
+	var builder s.Builder
+	for i := range strings {
+		_, err := builder.WriteString(strings[i])
+		if err != nil {
+			return nil, err
+		}
+		_, err = builder.WriteString(sep)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := builder.String()
+	return &result, nil
+}
+
+func CombineStringWithRuneSeparatorReferrer(sep *rune, strings *[]string) (*string, error) {
+	var builder s.Builder
+	for i := range *strings {
+		_, err := builder.WriteString((*strings)[i])
+		if err != nil {
+			return nil, err
+		}
+		_, err = builder.WriteRune(*sep)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := builder.String()
+	return &result, nil
+}
+func CombineStringWithStringSeparatorReferrer(sep *string, strings *[]string) (*string, error) {
+	var builder s.Builder
+	for i := range *strings {
+		_, err := builder.WriteString((*strings)[i])
+		if err != nil {
+			return nil, err
+		}
+		_, err = builder.WriteString(*sep)
 		if err != nil {
 			return nil, err
 		}
